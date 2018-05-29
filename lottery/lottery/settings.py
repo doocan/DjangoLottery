581c66696b95f0f88ce887512109d2cbfad73323
Durant <man_hattan@qq.com>
2018-05-29 14:09:20 +0800

    test mysql 5.7.20 for docker

--- lottery/lottery/settings.py
+++ lottery/lottery/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'lottery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'mysql',
+        'PORT': '3306'
     }
 }
 
